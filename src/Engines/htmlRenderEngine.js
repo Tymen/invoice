7ecd684230c23a911a7ec7aa538d83675b4d5465
Tymen <Tymenvis@outlook.com>
2021-03-27 14:44:01 +0100

    Total price can be calculated now

--- src/Engines/htmlRenderEngine.js
+++ src/Engines/htmlRenderEngine.js
@@ -4,16 +4,20 @@ var path = require('path')
 
 let setupTable = async (getTemplatePath, data, config) => {
     let getTableConfig = fs.readFileSync(getTemplatePath + "/table.txt", 'utf8')
-    let tableData = "";
+    let tableData = [];
     let cache = getTableConfig;
+    let total = 0;
     for(var key of Object.keys(data.tableData)){
         cache = getTableConfig
         for(var itemKey of Object.keys(data.tableData[key])){
            cache = cache.replace("${row_" + config[itemKey] + "}", data.tableData[key][itemKey])
+           if (itemKey == "price"){
+                total = total + parseFloat(data.tableData[key][itemKey].replace(/,/g, '.'))
+           }
         }
-        tableData = tableData + cache;
+        tableData[0] = tableData[0] + cache;
     }
-    console.log(tableData);
+    tableData[1] = total;
     return tableData
 }
 
@@ -21,31 +25,32 @@ module.exports = {
     async renderHTML(selectedTemplate, data) {
         let templatePath = env.PATH + "/template2.html"
 
-        try {
-
-            let getPath = process.cwd() + "/src/templates/";
-            let getTemplatePath = getPath + selectedTemplate;
-            let rawdata = fs.readFileSync(path.resolve(getTemplatePath + "/config.json"));
-            let getProperties = JSON.parse(rawdata);
-            let template = fs.readFileSync(getTemplatePath + "/template.txt", 'utf8')
-            for (var key of Object.keys(getProperties)) {
-                if (getProperties[key].variables){
-                    for(var x = 0; x < getProperties[key].variables.length; x++){
-                        template = template.replace("${" + getProperties[key].variables[x] + "}", data[getProperties[key]["name"]])
-                    }
-                }
-                if (getProperties[key].type === "table"){
-                    let getTableData = await setupTable(getTemplatePath, data, getProperties[key].variables)
-                    template = template.replace("${" + getProperties[key].title + "}", getTableData)
+        let getPath = process.cwd() + "/src/templates/";
+        let getTemplatePath = getPath + selectedTemplate;
+        let rawdata = fs.readFileSync(path.resolve(getTemplatePath + "/config.json"));
+        let getProperties = JSON.parse(rawdata);
+        let template = fs.readFileSync(getTemplatePath + "/template.txt", 'utf8')
+        for (var key of Object.keys(getProperties)) {
+            if (getProperties[key].variables){
+                for(var x = 0; x < getProperties[key].variables.length; x++){
+                    template = template.replace("${" + getProperties[key].variables[x] + "}", data[getProperties[key]["name"]])
                 }
             }
-            fs.writeFileSync(templatePath, template);
-
-        }catch(err){
-
-            throw err
-            
+            if(getProperties[key].type === "date"){
+                var today = new Date();
+                var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+                template = template.replace("${date}", date)
+            }
+            if (getProperties[key].type === "table"){
+                let getTableData = await setupTable(getTemplatePath, data, getProperties[key].variables)
+                template = template.replace("${" + getProperties[key].title + "}", getTableData[0])
+                template = template.replace("${subtotal}", `&euro; ${getTableData[1].toFixed(2)}`)
+                let getbtw = (getTableData[1] * 0.21).toFixed(2);
+                template = template.replace("${btw}", `&euro; ${getbtw}`)
+                template = template.replace("${total}", `&euro; ${(parseFloat(getTableData[1]) + parseFloat(getbtw).toFixed(2))}`)
+            }
         }
+        fs.writeFileSync(templatePath, template);
 
         return templatePath;
     }
